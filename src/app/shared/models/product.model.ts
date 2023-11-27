a66cb0865556c01a029baeb5fd6264e910b7faa8
niculuka <niculuka@yahoo.com>
2023-11-27 21:20:21 +0200

    update 27-11-2023 3

--- src/app/shared/models/product.model.ts
+++ src/app/shared/models/product.model.ts
@@ -1,11 +1,65 @@
 import { CATEGORY } from "../enums/electro.enum";
 
+// P r o d u c t  -  G a l l e r y --------------------------------
 export class ProductGallery {
     id: number | undefined;
     image: string = "";
     productId: number | undefined;
 }
 
+// P r o d u c t  -  D e s c r i p t i o n ------------------------
+export class ProductDescription {
+    id: number | undefined;
+    title: string = "";
+    text: string = "";
+    image: string = "";
+    productId: number | undefined;
+}
+
+// P r o d u c t  -  S p e c i f i c a t i o n ---------------------
+export class Processor {
+    producer?: CATEGORY;
+    type?: CATEGORY;
+    model?: CATEGORY;
+    frequency?: CATEGORY;
+    power?: CATEGORY;
+}
+
+export class Display {
+    diagonal?: CATEGORY;
+    frequency?: CATEGORY;
+    touchScreen: boolean = false;
+}
+
+export class Memory {
+    capacity?: CATEGORY;
+    type?: CATEGORY;
+    frequency?: CATEGORY;
+}
+
+export class HardDisk {
+    type?: CATEGORY;
+    capacity?: CATEGORY;
+    slot?: CATEGORY;
+}
+
+export class Connectivity {
+    slot?: CATEGORY;
+    touchScreen: boolean = false;
+}
+
+// ----------------------------------------------------------------
+export class ProductSpecification {
+    id: number | undefined;    
+    processor: Array<Processor> = [];
+    display: Array<Display> = [];
+    memory: Array<Memory> = [];
+    hardDisk: Array<HardDisk> = [];
+    connectivity: Array<Connectivity> = [];
+    productId: number | undefined;
+}
+
+// P R O D U C T  - - - - - - - - - - - - - - - - - - - - - - - - -
 export class Product {
     id: number | undefined;
     name: string = "";
@@ -27,6 +81,8 @@ export class Product {
     capacity?: CATEGORY;
     connect?: CATEGORY;
     gallery: Array<ProductGallery> = [];
+    descriptions: Array<ProductDescription> = [];
+    specifications: Array<ProductSpecification> = [];
 
     constructor(
         id?: number,
@@ -47,6 +103,8 @@ export class Product {
         capacity?: CATEGORY,
         connect?: CATEGORY,
         gallery: Array<ProductGallery> = [],
+        descriptions: Array<ProductDescription> = [],
+        specifications: Array<ProductSpecification> = [],
     ) {
         this.id = id;
         this.name = name;
@@ -66,6 +124,8 @@ export class Product {
         this.capacity = capacity;
         this.connect = connect;
         this.gallery = gallery;
+        this.descriptions = descriptions;
+        this.specifications = specifications;
     }
 }
 
